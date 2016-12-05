67a07aca1072867d47300af283159c8c282285ca
Mackie Loeffel <mackie.loeffel@web.de>
2016-12-05 13:19:55 +0100

    added checks for null in AbstractCoordinate

--- src/main/java/org/wahlzeit/model/AbstractCoordinate.java
+++ src/main/java/org/wahlzeit/model/AbstractCoordinate.java
@@ -5,14 +5,22 @@ public abstract class AbstractCoordinate implements Coordinate {
 
     @Override
     public boolean isEqual(Coordinate other) {
+        assertNotNull(other);
         return Math.abs(getDistance(other)) < EPSILON;
     }
 
     @Override
     public double getDistance(Coordinate other) {
+        assertNotNull(other);
         double dx = asCartesianX() - other.asCartesianX();
         double dy = asCartesianY() - other.asCartesianY();
         double dz = asCartesianZ() - other.asCartesianZ();
         return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    private void assertNotNull(Coordinate other) throws IllegalArgumentException {
+        if(other == null) {
+            throw new IllegalArgumentException("other must not be null");
+        }
     }
 }
